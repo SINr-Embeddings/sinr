91a08de48317e5d3cdd28ec25cd9d658fac09142
anthony <anthony.perez@univ-orleans.fr>
2023-02-08 18:01:11 +0100

    plugging back last graph

--- sinr/diachro/main.py
+++ sinr/diachro/main.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
             for i,graph in enumerate(graphs):
                 sinr_objects.append(SINr.load_from_graph(nk.nxadapter.nx2nk(graph)))
                 print(len(graphs),i,len(graph.nodes),len(graph.edges))
-            #test_diachrony(diachrony.DiachronicModels.static_factory,sinr_objects[:-1],dataset)
-            #test_diachrony(diachrony.DiachronicModels.multilayer_factory,sinr_objects[:-1],dataset)
-            test_diachrony(diachrony.DiachronicModels.multislices_factory,sinr_objects[:-1],dataset)
+            #test_diachrony(diachrony.DiachronicModels.static_factory,sinr_objects,dataset)
+            #test_diachrony(diachrony.DiachronicModels.multilayer_factory,sinr_objects,dataset)
+            test_diachrony(diachrony.DiachronicModels.multislices_factory,sinr_objects,dataset)
