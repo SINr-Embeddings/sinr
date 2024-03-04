6d9857141d78596967c653c41eace086a6675e3b
Anna B <72624798+aberanger@users.noreply.github.com>
2024-03-04 14:54:10 +0100

    73 wrong community memberships update when filtering dimensions (#75)
    * update of community_membership when filtering dimensions
    
    * sinr filtered: removing dimensions and updating communities_sets
    
    * fixed code to pass tests
    
    * comments

--- tests/test_sinr_dim_filter.py
+++ tests/test_sinr_dim_filter.py
@@ -35,29 +35,62 @@ class TestSinr_embeddings(unittest.TestCase):
         vec_remove=ge.InterpretableWordsModelBuilder(sinr, 'test', n_jobs=8, n_neighbors=25).build()
         
         self.vec_rm = vec_remove
+        
+        # vectors :
+        #[[0.         0.5        0.         0.         0.5       ]
+        # [0.25       0.         0.25       0.25       0.25      ]
+        # [0.         0.33333333 0.         0.33333333 0.33333333]
+        # [0.         0.33333333 0.33333333 0.         0.33333333]
+        # [0.25       0.25       0.25       0.25       0.        ]]
+        
+        # communities :
+        #[{0}, {1}, {2}, {3}, {4}]
+        
+        # community membership :
+        # [0, 1, 2, 3, 4]
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
     def test_remove_communities_dim_nnz(self):
-        
+        # Dimension 0 is removed
         self.vec_rm.remove_communities_dim_nnz(threshold_min = 3, threshold_max = 4)
+        
         self.assertTrue((np.round(self.vec_rm.vectors.toarray(), 2) == np.round(np.array([[0.5, 0., 0., 0.5],
                                                                      [0., 0.25, 0.25, 0.25],
                                                                      [0.33333333, 0., 0.33333333, 0.33333333],
                                                                      [0.33333333, 0.33333333, 0., 0.33333333],
                                                                      [0.25, 0.25, 0.25, 0.]]), 2)).all())
-        self.assertTrue(self.vec_rm.communities_sets == [{1}, {2}, {3}, {4}])
-        self.assertTrue(self.vec_rm.community_membership == [1, 2, 3, 4])
-                        
+        
+        # Community at index 0 is removed
+        self.assertTrue((self.vec_rm.communities_sets == [{1}, {2}, {3}, {4}]).all())
+        # Object member of the community 0 is no longer member of a community
+        self.assertTrue(self.vec_rm.community_membership == [-1, 0, 1, 2, 3])
+        
+        # Current vectors :
+        #[[0.5        0.         0.         0.5       ]
+        # [0.         0.25       0.25       0.25      ]
+        # [0.33333333 0.         0.33333333 0.33333333]
+        # [0.33333333 0.33333333 0.         0.33333333]
+        # [0.25       0.25       0.25       0.        ]]
+        
+        # communities :
+        #[{1}, {2}, {3}, {4}]
+        
+        # community membership :
+        # [-1, 0, 1, 2, 3]
+        
+        # Dimensions 0 and 3 are removed
         self.vec_rm.remove_communities_dim_nnz(threshold_min = 3, threshold_max = 3)
         self.assertTrue((np.round(self.vec_rm.vectors.toarray(), 2) == np.round(np.array([[0., 0.], 
                                                                       [0.25, 0.25],
                                                                       [0., 0.33333333],
                                                                       [0.33333333, 0.],
                                                                       [0.25, 0.25]]), 2)).all())
-        self.assertTrue(self.vec_rm.communities_sets == [{2}, {3}])
-        self.assertTrue(self.vec_rm.community_membership == [2, 3])
+        # Communities at index 0 and 3 are removed
+        self.assertTrue((self.vec_rm.communities_sets == [{2}, {3}]).all())
+        # Objects members of the communities 0 and 3 are no longer member of a community 
+        self.assertTrue(self.vec_rm.community_membership == [-1, -1, 0, 1, -1])
 
 
 if __name__ == '__main__':
