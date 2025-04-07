ed26e0aa77d87bba25138f762c8201b17a3d431b
Loniime <allaguirabia77@gmail.com>
2025-04-07 12:44:21 +0200

    Update

--- tests/test_sinr_evaluate.py
+++ tests/test_sinr_evaluate.py
@@ -7,7 +7,7 @@ import pytest
 import unittest
 
 import sinr.graph_embeddings as ge
-from sinr.text.evaluate import identify_gender_direction_sinr, calc_direct_bias_sinr, load_config
+from sinr.text.evaluate import fetch_data_MEN, fetch_data_WS353, eval_similarity, similarity_MEN_WS353_SCWS, vectorizer, clf_fit, clf_score, calcul_analogy_normalized, calcul_analogy_sparsified_normalized, identify_gender_direction_sinr, calc_direct_bias_sinr, load_config
 import urllib.request
 import os
 
