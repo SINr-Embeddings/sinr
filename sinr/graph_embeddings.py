6dc164ba6dc1ec95b9a3f00a5bae2a736ebe217c
sedraalhallak <a.sedra0201@gmail.com>
2025-07-21 10:45:33 +0200

    fixing adding comments for the OOV functions that add nodes and edges

--- sinr/graph_embeddings.py
+++ sinr/graph_embeddings.py
@@ -99,23 +99,23 @@ class SINr(object):
         logger.info("Finished building graph.")
         return cls(graph, out_of_LgCC, word_to_idx)
 
-        @classmethod
-        def _ensure_nodes(cls, graph, ids):
-            """Ensures that the nodes exist in the graph
+    @classmethod
+    def _ensure_nodes(cls, graph, ids):
+        """Ensures that the nodes exist in the graph
             Parameters:
             graph: Networkit graph .
             ids (Iterable[int]): A list or set of node IDs to ensure presence in the graph.
             
             Returns :
                 None.
-            """
-            for nid in ids:
-                if graph.hasNode(nid):
-                    continue
-                if nid < graph.upperNodeIdBound():
-                    graph.restoreNode(nid)
-                else:
-                    graph.addNodes(nid - graph.upperNodeIdBound() + 1)
+        """
+        for nid in ids:
+            if graph.hasNode(nid):
+                continue
+            if nid < graph.upperNodeIdBound():
+                graph.restoreNode(nid)
+            else:
+                graph.addNodes(nid - graph.upperNodeIdBound() + 1)
 
     def run(self, algo=None):
         """Runs the training of the embedding, i.e. community detection + vectors extraction
